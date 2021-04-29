43cdeb124c14b0389e3db476f4756aacf1a94021
kanishkalearn <60187526+kanishkalearn@users.noreply.github.com>
2021-04-29 10:27:58 -0700

    do

---
+++ src/app/auth/signup/signup.component.ts
@@ -0,0 +1,21 @@
+import { NgForm } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-signup',
+  templateUrl: './signup.component.html',
+  styleUrls: ['./signup.component.scss']
+})
+export class SignupComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onSignup(form:NgForm){
+
+    
+  }
+
+}
